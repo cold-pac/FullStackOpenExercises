ce8e10730e071c167da7f7ebdcba9a85d1697d9f
cold-pac <erol.a.cimen@gmail.com>
2020-03-01 22:44:27 +1100

    finished data for countries, part 1

---
+++ part2/countries/src/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+
+    const [countries, setCountries] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+
+    useEffect(() => {
+        axios
+            .get("https://restcountries.eu/rest/v2/all")
+            .then((response) => {
+                console.log(response.data)
+                setCountries(response.data)
+            }
+            )
+    }, [])
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    let countryContainer; 
+
+    const yourSearch = (searcher) => {
+        if (searcher === '') {
+            return 
+        }
+        countryContainer = [];
+        countries.forEach((elem) => {
+            if (elem.name.includes(searchTerm) || elem.name.toLowerCase().includes(searchTerm)) {
+                countryContainer.push(elem);
+            }
+        });
+        if (countryContainer.length === 1) {
+            return (
+                <div> 
+                    <h1>{countryContainer[0].name}</h1>
+                    <div>capital: {countryContainer[0].capital}</div>
+                    <div>population: {countryContainer[0].population}</div>
+                    {console.log(countryContainer[0].languages[0])}
+                    <h2>languages</h2>
+                    <ul>
+                        {countryContainer[0].languages.map(elem => <li>{elem.name}</li>)}
+                    </ul>
+                    <img alt="flag" src = {countryContainer[0].flag} />
+                </div>
+            )
+        } else if (countryContainer.length <= 10) {
+            return countryContainer.map((elem,index) => <div key = {index}>{elem.name}</div>);
+        } else  { 
+            return "Too many matches, specify another filter";
+        }
+    }
+
+    return (
+        <div>
+            <span>Find Countries </span>
+            <input value = {searchTerm} onChange = {handleChange}/>
+            <div>{yourSearch(searchTerm)}</div>
+        </div>       
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
